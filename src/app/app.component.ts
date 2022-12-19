27e00667e2096fbb5b360e43798be0468c0c095c
paulisidore <paul_isidore@hotmail.com>
2022-12-19 03:57:27 +0000

    Utilisation du lecteur QrCode dans Angular
    Reste a implémenter dans le projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseConfig } from 'ngx-scanner-qrcode';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nabysyDeliverySystem';
+  public config: BaseConfig = {
+    isAuto: false,
+    isDraw: true,
+    isAlwaysEmit: true,
+    text: { font: '25px serif' }, // Hiden { font: '0px', bottom: 40 },
+    frame: { lineWidth: 8 },
+    medias: {
+      audio: false,
+      video: {
+        facingMode: 'environment', // Front and back camera { facingMode: front ? "user" : "environment" } 
+        width: { ideal: 800 },
+        height: { ideal: 600 }
+      }
+    }
+  };
+
+  cadreScanner: any;
+  public onEvent(e: any): void {
+    //Les données lut sont ici
+    //On arrete la lecture
+    if (this.cadreScanner){
+      this.stopScan(this.cadreScanner);
+    }
+    let qrInfo=e ;
+    console.log(qrInfo);
+    console.log('QrData= '+qrInfo.data);
+  }
+
+  public onError(e: any): void {
+    alert(e);
+  }
+
+  public handle(QrCadreObjet: any, fn: string): void {
+    QrCadreObjet[fn]().subscribe(console.log, console.error);
+  }
+
+  public startScan(QrCadreObjet: any){
+    this.cadreScanner=QrCadreObjet ;
+    QrCadreObjet['start']().subscribe(console.log, console.error);
+  }
+  public stopScan(QrCadreObjet: any){
+    QrCadreObjet['stop']().subscribe(console.log, console.error);
+  }
+  
 }
